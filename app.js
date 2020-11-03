5b40c629f978b67d418080a74579d446debb49ce
Raymond Karumba N <karumbaraymond@gmail.com>
2020-11-03 13:23:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const PORT = process.env.PORT || 5200;
 
-const targetBaseUrl = 'http://co-recipes.herokuapp.com';
+const targetBaseUrl = 'https://co-recipes.herokuapp.com';
 
 function handleRequest(req, res) {
 	const targetUrl = targetBaseUrl + req.originalUrl;
